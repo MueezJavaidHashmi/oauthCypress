937d237a04fbe7ce496ed4fac0d1ab3da8f784e0
MueezJavaidHashmi <mjh1230689@gmail.com>
2020-11-30 20:41:07 +0500

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const clickHandle = () => {
+    window.open("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78z5t2t8c4ko5b&redirect_uri=http://localhost:3000&state=fooobar&scope=r_liteprofile", "_blank")
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={clickHandle}>
+        Click Me
+      </button>
     </div>
   );
 }
